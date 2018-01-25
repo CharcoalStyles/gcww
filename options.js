2ac395d29c9e7629e36758d9991411245a772d7d
Aaron Charcoal Styles <aaron@charcoalstyles.com>
2018-01-25 22:49:43 +1100

    Now handles multiple wallets, and adds labels for the wallets. Also now uses explorer.grlc-bakery.fun

--- options.js
+++ options.js
@@ -1,27 +1,82 @@
+function addInput(label, wallet) {
+	var walletsDiv = document.getElementById('walletsList');
+
+	var labelTxt = document.createElement("input"); //input element, text
+	labelTxt.setAttribute('type', "text");
+	labelTxt.setAttribute('value', label);
+	labelTxt.style.cssText = "width: 160px; margin-top: 10px; margin-right: 10px;";
+	walletsDiv.appendChild(labelTxt);
+
+	var walletTxt = document.createElement("input"); //input element, text
+	walletTxt.setAttribute('type', "text");
+	walletTxt.setAttribute('value', wallet);
+	walletTxt.style.cssText = "width: 160px; margin-top: 10px; margin-right: 10px;";
+	walletsDiv.appendChild(walletTxt);
+	
+	var delWallet = document.createElement("span"); //input element, text
+	delWallet.style.cssText = "margin-top: 10px;";
+	delWallet.innerHTML = "X";
+	walletsDiv.appendChild(delWallet);
+	delWallet.addEventListener('click',
+		function () {
+			walletsDiv.removeChild(labelTxt);
+			walletsDiv.removeChild(walletTxt);
+			walletsDiv.removeChild(delWallet);
+	});
+}
+
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var walletId = document.getElementById('walletId').value;
-  chrome.storage.sync.set({
-    walletId: walletId
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+	var wallets = [];
+
+	var children = document.getElementById('walletsList').children;
+	
+	for (var i = 0; i < Math.floor(children.length / 2); i++) {
+		var label = children[i * 2];
+		var wallet = children[i * 2 + 1];
+		wallets.push({
+			label: label.value,
+			walletId: wallet.value
+		});
+	}
+	chrome.storage.sync.set({
+		walletId: '',
+		wallets: wallets
+	}, function () {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function () {
+			status.textContent = '';
+		}, 1500);
+	});
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get({
-    walletId: ''
-  }, function(items) {
-    document.getElementById('walletId').value = items.walletId;
-  });
+	chrome.storage.sync.get({
+		walletId: '',
+		wallets: [{
+				label: "",
+				walletId: ""
+			}
+		]
+	}, function (items) {
+		if (items.walletId) {
+			addInput("wallet", items.walletId);
+		} else {
+			for (var i = 0; i < items.wallets.length; i++) {
+				addInput(items.wallets[i].label, items.wallets[i].walletId);
+			}
+		}
+	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
-    save_options);
+	save_options);
+
+document.getElementById('addWallet').addEventListener('click',
+	function () {
+	addInput("", "");
+});
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var walletId = ""
+var wallets = []
 	document.addEventListener('DOMContentLoaded', function () {
 		var updateButton = document.getElementById('update');
 		updateButton.addEventListener('click', doUpdate, false);
@@ -9,46 +9,84 @@ var walletId = ""
 		}, false);
 
 		chrome.storage.sync.get({
-			walletId: ""
+			walletId: "",
+			wallets: [{
+					label: "",
+					walletId: ""
+				}
+			]
 		}, function (items) {
-			walletId = items.walletId;
+			if (items.walletId != '') {
+				wallets.push({
+					label: "wallet",
+					walletId: items.walletId
+				});
+			} else {
+				for (var i = 0; i < items.wallets.length; i++) {
+					wallets.push({
+						label: items.wallets[i].label,
+						walletId: items.wallets[i].walletId
+					});
+				}
+			}
 			doUpdate();
 		});
 	}, false);
+	
+function addLoadableWalletToTable(tableElem, label, showloader)
+{
+	// Create an empty <tr> element and add it to the 1st position of the table:
+	var row = tableElem.insertRow(0);
+
+	// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+	var labelCell = row.insertCell(0);
+	var balanceCell = row.insertCell(1);
+
+	// Add some text to the new cells:
+	labelCell.innerHTML = label;
+	if (showloader)
+		balanceCell.innerHTML = "<img src='\icon.png' class='image'>";//balance + "&#9404;";
+	return balanceCell;
+}
 
 function doUpdate() {
-	var balanceSpan = document.getElementById('balance');
+	var balanceTable = document.getElementById('BalanceTable');
+	balanceTable.innerHTML = "";
 
-	if (walletId.length == 0) {
-		balanceSpan.innerHTML = "No wallet set";
+	if (wallets.length == 1 && wallets[0] == '') {
+		addLoadableWalletToTable(balanceTable, "No wallet set", false);
 	} else {
-		var request = new XMLHttpRequest();
-		request.open('GET', 'http://52.89.91.13/ext/getbalance/' + walletId, true);
+		for (var i = 0; i < wallets.length; i++) {
+			var wallet = wallets[i];
+			
+			var request = new XMLHttpRequest();
+			request.open('GET', 'http://explorer.grlc-bakery.fun/ext/getbalance/' + wallet.walletId, true);
+			
+			var balElem = addLoadableWalletToTable(balanceTable, wallet.label, true);
 
-		balanceSpan.innerHTML = "<img src='\icon.png' class='image'>";
-
-		request.onload = function () {
-			var resp = request.responseText;
-			if (request.status >= 200 && request.status < 400) {
-				try {
-					var json = JSON.parse(request.responseText);
-					if ("error" in json) {
-						balanceSpan.innerHTML = json["error"];
-					} else {
-						balanceSpan.innerHTML = resp;
+			request.onload = function () {
+				var resp = request.responseText;
+				if (request.status >= 200 && request.status < 400) {
+					try {
+						var json = JSON.parse(request.responseText);
+						if ("error" in json) {
+							balElem.innerHTML = json["error"];
+						} else {
+							balElem.innerHTML = resp;
+						}
+					} catch (exception) {
+						balElem.innerHTML = resp + "&#9404;";
 					}
-				} catch (exception) {
-					balanceSpan.innerHTML = resp + "&#9404;";
+				} else {
+					balElem.innerHTML = "ERR";
 				}
-			} else {
-				balanceSpan.innerHTML = "ERR";
-			}
-		};
+			};
 
-		request.onerror = function () {
-			balanceSpan.innerHTML = "ERR";
-		};
+			request.onerror = function () {
+				balElem.innerHTML = "ERR";
+			};
 
-		request.send();
+			request.send();
+		}
 	}
 }
